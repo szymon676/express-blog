2f93749953fbd13bec08b8e4cede4c5b4083475c
Szymon <sgil73072@gmail.com>
2023-03-21 12:03:53 +0100

    add blogs json

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/', (request , response) => {
     response.send("Hello world")
 });
 
 
-app.listen(3000, () => console.log("3000"))
+app.listen(3000, () => console.log(`server listening on port 3000`));
