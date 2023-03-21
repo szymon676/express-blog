40e25c2c02eb13f1f88ed064b913f9ccfd8ec727
Hixo <hixohixo32@gmail.com>
2023-03-21 12:09:35 +0100

    szymoslav kid es

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ import posts from './data/posts.json'
 const app = express();
 
 app.get('/', (request , response) => {
-    response.send("Hello world")
+    response.json(posts)
 });
+
+app.get('/:Id', (request, response) => {
+    const {Id} = request.params
+    response.json({ post: posts.blogs.filter(post => post.id === +Id) })
+})
 
 
 app.listen(3000, () => console.log(`server listening on port 3000`));
